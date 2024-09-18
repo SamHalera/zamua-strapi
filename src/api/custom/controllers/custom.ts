6875af5e1e579b3f87fc97c69c12990332714673
SamHalera <samuel.halera@gmail.com>
2024-09-18 12:39:45 +0200

    custom route for projects and project by slug / create slug after project creation lifecycle

--- src/api/custom/controllers/custom.ts
+++ src/api/custom/controllers/custom.ts
@@ -13,6 +13,7 @@ export default {
           filters: {
             path: `${pathname}`,
           },
+
           populate: {
             dynamicZone: {
               populate: {
@@ -46,36 +47,39 @@ export default {
       ctx.body = err;
     }
   },
-};
 
-// populate: {
-//   dynamicZone: {
-//     populate: {
-//       hero: {
-//         populate: {
-//           optionsHero: true,
-//         },
-//       },
-//       musicFeatures: {
-//         populate: {
-//           image: true,
-//           musicFeaturesSingle: true,
-//         },
-//       },
-//     },
-//   },
-// },
-// {
-//   dynamicZone: {
-//     populate: "*",
-//     musicFeatures: {
-//       populate: {
-//         musicFeatureSingle: {
-//           populate: {
-//             image: true,
-//           },
-//         },
-//       },
-//     },
-//   },
-// },
+  async getProjects() {
+    const projects = await strapi.entityService.findMany(
+      "api::project.project",
+      {
+        publicationState: "live",
+        populate: {
+          imageCover: true,
+          project_members: true,
+        },
+      }
+    );
+
+    return projects;
+  },
+  async findProjectBySlug(ctx) {
+    const { slug } = ctx.query;
+
+    const project = await strapi.db.query("api::project.project").findOne({
+      where: {
+        slug,
+      },
+      populate: {
+        imageCover: true,
+        project_members: true,
+        Gallery: {
+          populate: {
+            image: true,
+          },
+        },
+      },
+    });
+
+    return project;
+  },
+};
